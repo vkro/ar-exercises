96d33ee57f9bb6bd63d3d404a5bf94f6ac9d3db2
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-11 04:19:43 +0000

    load subset of stores (plus ex1 cleanup)

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,13 +9,8 @@ puts "----------"
 class Store 
 end
 
-# annual_revenue 300000, men's and women's apparel
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-
-# annual revenue 1260000, women's apparel only
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
-
-# annual revenue 190000, carries men's apparel only
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.all.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: false, womens_apparel: false)
+yaletow = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where("mens_apparel = true")
+
+@mens_stores.each do |store|
+  puts "Store: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+
+@womens_stores_annual_revenue_under_1m = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+@womens_stores_annual_revenue_under_1m.each do |store|
+  puts "Store: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
